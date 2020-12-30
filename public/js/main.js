febc6c9856e2fda4cb49353f618c0d82f8bd8c5a
Alex <agasparr98@gmail.com>
2020-12-30 15:17:16 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// - # Exercice 4 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments
+
+let array = ["chose","truc","machin"];
+
+//     - ## Afficher les éléments de l'array
+
+console.log(array);
+
+//     - ## Ajouter et afficher votre prenom a l'array
+
+array.push("Alex");
+console.log(array);
+
+//     - ## Remplacer et afficher un élément de l'array par autre chose
+
+let i = 1;
+array[i] = "stinky";
+console.log(array);
+
+//     - ## Effacer un élément de l'array
+
+array.splice(2);
+
+//     - ## Afficher le contenu le d'array
+
+console.log(array);
